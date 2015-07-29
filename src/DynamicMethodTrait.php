3af15c9e436be0c4dcdf9bd2a1eb16fec04aa878
Mark Topper <mail@webman.io>
2015-07-29 10:34:28 +0200

    Update DynamicMethodTrait.php

--- src/DynamicMethodTrait.php
+++ src/DynamicMethodTrait.php
@@ -11,8 +11,16 @@ Trait DynamicMethodTrait {
         public function addMethod($methodName, $methodCallable)
         {
                 if (!is_callable($methodCallable)) {
-                        throw new InvalidArgumentException('Second param must be callable');
+                        throw new InvalidArgumentException('Second param must be callable.');
                 }
                 $this->methods[$methodName] = Closure::bind($methodCallable, $this, get_class());
+        }
+
+        public function removeMethod($methodName)
+        {
+                if (!isset($this->methods[$methodName])) {
+                        throw new InvalidArgumentException("Method [{$methodName}] does not exists.");
+                }
+                unset($this->methods[$methodName]);
         }
 }
