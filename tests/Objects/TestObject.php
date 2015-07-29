437be80c1e58784c80a754e304ae499d864b1d72
Mark Topper <mail@webman.io>
2015-07-29 20:30:43 +0200

    Update TestObject.php

--- tests/Objects/TestObject.php
+++ tests/Objects/TestObject.php
@@ -1,5 +1,5 @@
 <?php
 require_once __DIR__ . '/../../vendor/autoload.php';
 use Borter\SmartObject\Obj;
-class TestObjectWithGetterSetterDynamicMethodTrait extends Obj {
+class TestObject extends Obj {
 }
