25fc7e78e563efbb9dd6104500c61ad46455bf95
Mark Topper <mail@webman.io>
2015-07-29 14:55:25 +0200

    Update SmartObjectTest.php

--- tests/SmartObjectTest.php
+++ tests/SmartObjectTest.php
@@ -1,17 +1,6 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-
-use Borter\SmartObject\Obj;
-use Borter\SmartObject\GetterTrait;
-use Borter\SmartObject\SetterTrait;
-use Borter\SmartObject\DynamicMethodTrait;
-
-class TestObject extends Obj {
-  use GetterTrait;
-  use SetterTrait;
-  use DymanicMethodTrait;
-}
+require __DIR__ . '/TestObject.php';
 
 class SmartObjectTestCase extends PHPUnit_Framework_TestCase
 {
@@ -34,27 +23,20 @@ class SmartObjectTestCase extends PHPUnit_Framework_TestCase
     /**
      * @expectedException InvalidArgumentException
      */
+/*
     public function testAddMethodWithArray()
     {
         $smartobject = new TestObject();
         $smartobject->addMethod(array());
         $this->fail('Expecting exception when the constructor is passed an array');
     }
+*/
 
     public function testSetterTrait()
     {
-        $smartobject = TestObject();
+        $smartobject = new TestObject();
         $this->assertSmartObject($smartobject);
         $this->assertEquals('bar', (string) $smartobject->setFoo('bar'));
         $this->assertEquals('bar', (string) $smartobject->foo);
-    }
-
-    public function testChaining()
-    {
-        $smartobject = TestObject();
-        $this->assertSmartObject($smartobject);
-        $smartobject->setFoo('foo')->setBar('bar');
-        $this->assertEquals('foo', $smartobject->getFoo());
-        $this->assertEquals('bar', $smartobject->getBar());
     }
 }
