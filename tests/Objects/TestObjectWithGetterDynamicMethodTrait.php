abc5df2bcb2897c329255a93d984f2dd0e807e2a
Mark Topper <mail@webman.io>
2015-08-03 09:52:40 +0200

    Update TestObjectWithGetterDynamicMethodTrait.php

--- tests/Objects/TestObjectWithGetterDynamicMethodTrait.php
+++ tests/Objects/TestObjectWithGetterDynamicMethodTrait.php
@@ -9,4 +9,16 @@ class TestObjectWithGetterDynamicMethodTrait extends Obj
 {
     use GetterTrait;
     use DynamicMethodTrait;
+    
+    public $username;
+    
+    public function getProcessedAttribute()
+    {
+        return ucfirst($this->username);
+    }
+    
+    public function setProcessedAttribute($username)
+    {
+        $this->username = strtolower($username);
+    }
 }
